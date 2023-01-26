ba0e11856e6efa111781881709a55bdfc2d84cb1
laetitiarabibizaka <laetitiarabibizaka@gmail.com>
2023-01-26 16:05:23 +0300

    integration inscription client

--- app-angular/src/app/services/utilisateur/utilisateur.service.ts
+++ app-angular/src/app/services/utilisateur/utilisateur.service.ts
@@ -16,4 +16,15 @@ export class UtilisateurService {
 		};
 		return this.http.post(`${environment.baseUrl}client/traitementLogin`,data,{});
 	}
+
+	signup(nom:string,prenom:string,contact:string,login:string,mdp:string) : Observable<any>{
+		const data:any={
+			nom: nom,
+			prenom: prenom,
+			contact: contact,
+			login: login,
+			mdp: mdp
+		};
+		return this.http.post(`${environment.baseUrl}client/createUser`,data,{});
+	}
 }
