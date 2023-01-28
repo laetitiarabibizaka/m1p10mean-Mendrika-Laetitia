721c639600f3d1f337a33d2fa5f9aa2ee2c41185
mendrika-ops <mendrikaandriamitantsoa@gmail.com>
2023-01-28 09:21:37 +0300

    deconnexion user

--- app-angular/src/app/components/client/menubar/menubar.component.ts
+++ app-angular/src/app/components/client/menubar/menubar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menubar',
@@ -7,9 +8,14 @@ import { Component } from '@angular/core';
 })
 export class MenubarComponent {
   username: string = ''
+  constructor(private router: Router){}
   ngOnInit(){
     var user = JSON.parse(sessionStorage.getItem("sessionUser") as any);
     var data=user.prenom;
     this.username = data
   } 
+  deconnexion() {
+    sessionStorage.clear()
+    this.router.navigate(['/']);
+  }
 }
