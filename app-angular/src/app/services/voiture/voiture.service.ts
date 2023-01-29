f747703c85ef522d4afc4ba99e7fe3fe51fb182b
laetitiarabibizaka <laetitiarabibizaka@gmail.com>
2023-01-29 20:38:44 +0300

    Update voiture.service.ts

--- app-angular/src/app/services/voiture/voiture.service.ts
+++ app-angular/src/app/services/voiture/voiture.service.ts
@@ -35,5 +35,46 @@ export class VoitureService {
     return this.http.get(`${environment.baseUrl}client/recherchevoiture/${user}/${matricule}`,{});
   }
 
+  validerDepositionVoiture(login: string,numero: string,dateDepot: string){
+    var data : any = {
+      login: login,
+      numero: numero,
+      dateDepot: dateDepot
+    }
+    return this.http.put(`${environment.baseUrl}admin/ajoutReparation`,data);
+  }
+
+  ajouterReparation(numero: string,dateDepot: string,desce: string,pu: Number,qte: Number){
+    var data: any = {
+      numero: numero,
+      dateDepot: dateDepot,
+      desce: desce,
+      pu: pu,
+      qte: qte
+    }
+    return this.http.put(`${environment.baseUrl}admin/terminerReparation`,data);
+  }
+
+  terminerReparation(numero: string,dateDepot: string,desce: string){
+    var data: any = {
+      numero: numero,
+      dateDepot: dateDepot,
+      desce: desce
+    }
+    return this.http.put(`${environment.baseUrl}admin/terminerReparation`,data);
+  }
+
+  changerEtatDeposition(numero: string, dateDepot: string,etat: Number){
+    var data: any = {
+      numero: numero,
+      dateDepot: dateDepot,
+      etat: etat
+    }
+    return this.http.put(`${environment.baseUrl}admin/changerEtatDeposition`,data);
+  }
+
+  envoyerEmail(login: string){
+    return this.http.post(`${environment.baseUrl}admin/envoieMail`,{login: login});
+  } 
   
 }
